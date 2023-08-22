b3691cddd291d420dfd973a99e35c0ca24eb562f
TriMadon <mehammed6789@gmail.com>
2023-08-22 17:53:01 +0300

    Improve implementation

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,35 +1,13 @@
 def stock_picker(stock_prices)
-  day_pair = []
+  pair_combos = stock_prices.combination(2).to_a
+  max_profit_combo = pair_combos.max_by { |buy, sell| sell - buy }
+  buy_day = stock_prices.index(max_profit_combo[0])
+  sell_day = stock_prices.rindex(max_profit_combo[1])
 
-  price_day_map = stock_prices.each_with_index.reduce(Hash.new { |hash, key| hash[key] = [] }) do |hash, (price, day)|
-    hash[price].push(day)
-    hash
-  end
-
-  max_price_diff = 0
-
-  stock_prices.uniq.sort.reverse.each_with_index do |price, i|
-    max_day = price_day_map[price].max
-    max_price = price
-    min_day = nil
-
-    next if(max_day == 0)
-
-    min_price = stock_prices.take(max_day).min
-    min_day = stock_prices.take(max_day).find_index(min_price)
-
-    price_diff = max_price - min_price
-
-    if price_diff > max_price_diff
-      max_price_diff = price_diff
-      day_pair = [min_day, max_day]
-    end
-  end
-
-  day_pair
+  [buy_day, sell_day]
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 # => [1,4]
-p stock_picker([17,3,6,9,15,8,6,20,12,14,2,9,18,5])
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 20, 12, 14, 2, 9, 18, 5])
 # => [1,7]
