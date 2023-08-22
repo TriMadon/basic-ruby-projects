107f6c8fc17c6a41f69bea082eac5da26f73fc15
TriMadon <mehammed6789@gmail.com>
2023-08-22 12:21:32 +0300

    Add implementation

---
+++ caesar-cipher/ceasar_cipher.rb
@@ -0,0 +1,15 @@
+def caesar_cipher(string, shift_factor)
+  regex = /[a-zA-Z]/
+  shift_factor %= 26
+  char_arr = string.chars.map do |char|
+    regex =~ char ? shift_letter(char, shift_factor) : char
+  end
+  char_arr.join('')
+end
+
+def shift_letter(letter, shift)
+  base_ord = letter.upcase == letter ? 'A'.ord : 'a'.ord
+  ((letter.ord + shift - base_ord) % 26 + base_ord).chr
+end
+
+puts caesar_cipher('What a string!', -15) # => "Bmfy f xywnsl!"
